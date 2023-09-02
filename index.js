6e7040356b2d7905b74c74f7fc0850897e630154
Joaquin Llarena <joa.llarena04@gmail.com>
2023-09-01 22:00:52 -0300

    final

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector('.container-cart-products');
 
@@ -20,6 +26,9 @@ const saveProductsToJSON = () => {
     localStorage.setItem('cartProducts', productsJSON);
 };
 
+
+
+
 // Función para cargar productos desde JSON
 const loadProductsFromJSON = () => {
     const productsJSON = localStorage.getItem('cartProducts');
@@ -28,6 +37,8 @@ const loadProductsFromJSON = () => {
         eventoHtml();
     }
 };
+
+
 
 // Cargar productos guardados al cargar la página
 window.addEventListener('DOMContentLoaded', loadProductsFromJSON);
@@ -59,6 +70,9 @@ productList.addEventListener('click', (x) => {
     }
 });
 
+
+//carrito 
+
 const eventoHtml = () => {
     rowProduct.innerHTML = '';
 
@@ -80,18 +94,98 @@ const eventoHtml = () => {
             </svg>
         `;
 
-        rowProduct.append(containerProducts);
+        rowProduct.appendChild(containerProducts);
 
         total = total + parseInt(product.cantidad * product.precio.slice(1));
         totalProducts = totalProducts + product.cantidad;
+
+        const deleteIcon = containerProducts.querySelector('.icon-close');
+
+        // ...
+
+        deleteIcon.addEventListener('click', () => {
+            const index = allProducts.indexOf(product);
+            if (index !== -1) {
+                const deletedProduct = allProducts.splice(index, 1)[0]; // Obtén el producto eliminado
+                eventoHtml();
+                saveProductsToJSON();
+
+                // Muestra una alerta para informar al usuario que el producto se eliminó
+                
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `Se ha eliminado "${deletedProduct.titulo}" del carrito.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        });
+
+        // ...
+
     });
 
     valorTotal.innerText = `$${total}`;
     countProducts.innerText = totalProducts;
-
-
-
-
 };
+
+
+
+
+
+//darkmode 
+
+
+const darkMode = document.querySelector(".btnn");
+const body = document.body
+const carrito = document.querySelector(".cart-product")
+const carritoItems = document.querySelector(".cart-total")
+
+darkMode.addEventListener("click", modoOscuro)
+
+function modoOscuro() {
+
+    body.classList.toggle("dark-mode");
+    carrito.classList.toggle("dark-mode-cart");
+
+
+    darkMode.innerText = ("tocame para modo claro");
+
+
+    if (body.classList.contains("dark-mode")) [
+
+        darkMode.innerText = ("modo claro")
+
+    ]
+    else (darkMode.innerText = ("modo oscuro"))
+
+
+}
+
+const accessKey = 'y-t_v2jAD71auz7_kRELkqJP1Z9y4Oq6iqdwpRx5tb0'; // Reemplaza con tu clave de acceso
+const query = 'gorras'; // Tu consulta de búsqueda
+const imageContainer = document.querySelector('.container-de-imagenes'); // Reemplaza con el selector de tu contenedor de imágenes
+
+fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${accessKey}`)
+  .then(response => response.json())
+  .then(data => {
+    // Procesa los datos y muestra las imágenes en tu sitio web
+    const images = data.results; // Obtiene la lista de imágenes de los resultados
+
+    images.forEach(imageData => {
+      const imageElement = document.createElement('img'); // Crea un elemento de imagen
+      imageElement.src = imageData.urls.regular; // Establece la URL de la imagen
+      imageElement.alt = imageData.alt_description; // Establece un texto alternativo para la imagen
+
+      // Agrega la imagen al contenedor de imágenes en tu página
+      imageContainer.appendChild(imageElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error al cargar imágenes de Unsplash:', error);
+  });
+
+
 
 
